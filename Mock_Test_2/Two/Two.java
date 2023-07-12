b8be3a665b52452132e63bf30e5a0fa3d1cd80ec
Arnab Mahato <arnabmahato1010@gmail.com>
2023-07-12 23:12:46 +0530

    Added user input

--- Mock_Test_2/Two/Two.java
+++ Mock_Test_2/Two/Two.java
@@ -1,51 +1,80 @@
-abstract class Shape{
-    public abstract String calculateArea();
+import java.util.Scanner;
+
+interface Shape{
+    String calculateArea();
 }
 
-class Rectangle extends Shape{
-    @Override
-    public String calculateArea() {
-        float length = 40.56F, breadth = 30.67F;
-        return ("The area of Rectangle is: " + length * breadth);
+class Rectangle implements Shape{
+
+    float length, breadth;
+
+    Rectangle(float length, float breadth){
+        this.length = length;
+        this.breadth = breadth;
     }
-}
-
-class Circle extends Shape{
-    @Override
-    public String calculateArea() {
-        float pi = 3.14F, radius = 3;
-        return ("The area of Circle is: " + pi * radius * radius);
+    public String calculateArea(){
+        return("The Area of Rectangle is: " + length * breadth);
     }
+
 }
 
-class Triangle extends Shape{
-    @Override
-    public String calculateArea() {
-        float base = 13, height = 8;
-        return ("The area of Triangle is: " + (float)1/2 * base * height);
+class Circle implements Shape{
+
+    final float pi = 3.14F;
+    float radius;
+
+    Circle(float radius){
+        this.radius = radius;
+    }
+    public String calculateArea(){
+        return("The Area of Circle is: " + pi * radius * radius);
+    }
+
+}
+
+class Triangle implements Shape{
+
+    float base, height;
+
+    Triangle(float base, float height){
+        this.base = base;
+        this.height = height;
+    }
+    public String calculateArea(){
+        return("The Area of Triangle is: " + ((float)1/2 * base * height));
     }
 
 }
 
 class ShapeCalculator{
-
-    public void printArea(Shape shape){
-
+    void printArea(Shape shape){
         System.out.println(shape.calculateArea());
     }
-
 }
 
 
 public class Two {
-
     public static void main(String[] args) {
-
         ShapeCalculator s = new ShapeCalculator();
+        Scanner sc = new Scanner(System.in);
 
-        s.printArea(new Rectangle());
-        s.printArea(new Circle());
-        s.printArea(new Triangle());
+        //Rectangle
+        System.out.print("Enter length of Rectangle: ");
+        float l = sc.nextFloat();
+        System.out.print("Enter breadth of Rectangle: ");
+        float b = sc.nextFloat();
+        s.printArea(new Rectangle(l,b));
 
+        //Circle
+        System.out.print("\nEnter radius of Circle: ");
+        float r = sc.nextFloat();
+        s.printArea(new Circle(r));
+
+        //Triangle
+        System.out.print("\nEnter base of Triangle: ");
+        float base = sc.nextFloat();
+        System.out.print("Enter height of Triangle: ");
+        float height = sc.nextFloat();
+        s.printArea(new Triangle(base,height));
     }
 }
